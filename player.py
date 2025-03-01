a3ab70aa670b7f8e8821629f345dae378912e72c
Omar Salama <salamaom@sheridancollege.ca>
2025-03-01 11:00:00 -0500

    defined roll for a dice roll from 1-6

--- player.py
+++ player.py
@@ -6,4 +6,8 @@ class Player:
     def __init__(self):
         # Player starts with score 0
         self.score = 0  
+
+    def roll(self):
+        # using dice roll ( from 1-6)
+        return random.randint(1, 6)
     
