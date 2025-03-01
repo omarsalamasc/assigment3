863096ce8cdefbdc74a65bc355ced3508c84574f
Omar Salama <salamaom@sheridancollege.ca>
2025-03-01 12:10:00 -0500

    Defined start() method set random number 1-6 and guess

--- game.py
+++ game.py
@@ -7,3 +7,8 @@ class Game:
     def __init__(self):
         self.players = [Player(), Player(), Player()]
         self.guess = 0  # initialized to 0 initially
+
+    def start(self):
+        # Set a random guess number between 1 and 6
+        self.guess = random.randint(1, 6)
+        print("[Game] Guess number for this round is " + str(self.guess))
